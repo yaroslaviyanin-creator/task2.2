f0b8aac899233b4fd7055d6205087a41c9db5fa9
Янин Ярослав <yaroslav.i.yanin@gmail.com>
2026-07-13 04:52:01 +0500

    Реализован парсинг аргументов через getopt с проверками и тестовым выводом

--- app/src/main.c
+++ app/src/main.c
@@ -30,5 +30,66 @@ int main(int argc, char* argv[]) {
         .dir = NULL,
         .format_str = NULL
     };
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, "i:o:l:g:n:d:f:")) != -1) {
+        switch (opt) {
+        case 'i':
+            cfg.file_name = optarg;
+            break;
+        case 'o':
+            if (sscanf(optarg, "%d", &cfg.offset) != 1) {
+                fprintf(stderr, "Error: Option -o requires an integer argument.\n");
+                return 1;
+            }
+            break;
+        case 'l':
+            if (sscanf(optarg, "%d", &cfg.length) != 1) {
+                fprintf(stderr, "Error: Option -l requires an integer argument.\n");
+                return 1;
+            }
+            break;
+        case 'g':
+            if (sscanf(optarg, "%d", &cfg.group_size) != 1) {
+                fprintf(stderr, "Error: Option -g requires an integer argument.\n");
+                return 1;
+            }
+            break;
+        case 'n':
+            if (sscanf(optarg, "%d", &cfg.count) != 1) {
+                fprintf(stderr, "Error: Option -n requires an integer argument.\n");
+                return 1;
+            }
+            break;
+        case 'd':
+            cfg.dir = optarg;
+            break;
+        case 'f':
+            cfg.format_str = optarg;
+            break;
+        default:
+            fprintf(stderr, "Error: invalid arguments.\n");
+            return 1;
+        }
+    }
+
+    // Проверка, есть ли имя файла, либо директория
+    if (cfg.file_name == NULL && cfg.dir == NULL) {
+        fprintf(stderr, "Error: input file (-i) or directory (-d) must be specified.\n");
+        return 1;
+    }
+
+    // Тест
+    printf("File:        %s\n", cfg.file_name ? cfg.file_name : "(not set)");
+    printf("Directory:   %s\n", cfg.dir ? cfg.dir : "(not set)");
+    printf("Offset:      %d\n", cfg.offset);
+    printf("Length:      %d\n", cfg.length);
+    printf("Group size:  %d\n", cfg.group_size);
+    printf("Count:       %d\n", cfg.count);
+    printf("Format:      %s\n", cfg.format_str ? cfg.format_str : "(standard)");
+
+    // ...
+
     return 0;
 }
